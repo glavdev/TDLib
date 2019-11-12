b94d7fc67ea2672c960f7fa7b3b7a0147e6767fb
SergeChepikov <mr.chepikov@gmail.com>
2019-11-12 14:39:55 +0300

    правки

--- tests/functional/Integration/CommonCreateRecord/CommonCreatedOrderTest.php
+++ tests/functional/Integration/CommonCreateRecord/CommonCreatedOrderTest.php
@@ -18,10 +18,10 @@ class CommonCreatedOrderTest extends Unit
      */
     protected $tester;
 
-    protected function setUp()
+    protected function _before()
     {
-        parent::setUp();
-        getDB()->query("SET FOREIGN_KEY_CHECKS=0;
+        $this->tester->haveInDatabase("orders", ['td_id' => 15]);
+        /*getDB()->query("SET FOREIGN_KEY_CHECKS=0;
         INSERT INTO `orders`
         (`td_id`, `sku`, `serv`, `gp_status`, `td_status_id`, `td_status_name`, `shipment_id`, `return_shipment_id`,
         `barcode`, `price`, `td_status`, `payment_type`, `create_date`, `modified_date`, `pkg_partial`,
@@ -29,7 +29,7 @@ class CommonCreatedOrderTest extends Unit
         `comment`, `dst_punkt_id`, `items_count`, `partial_giveout_enabled`, `can_open_box`) VALUES
         (2026871, 'TEST-PKG3-01102019-1606',	'выдача',	'transfering',	3,	'Получен в ТД',	308896,	NULL,
         '6*TEST-PKG3-01102019-1606',	2000,	NULL,	NULL,	'2019-10-04 12:08:07',	'2019-10-04 12:52:16',	NULL,
-        200,	2,	'Тест Тестов',	'89991112233',	'',	'0000-00-00',	'',	'Avtovo-S75',	'1',	1,	1);");
+        200,	2,	'Тест Тестов',	'89991112233',	'',	'0000-00-00',	'',	'Avtovo-S75',	'1',	1,	1);");*/
     }
 
     protected function _after()
